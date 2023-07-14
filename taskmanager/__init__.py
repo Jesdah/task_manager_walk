ef5c22fd8ff03038154dc1832df9d53bfb194364
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-07-14 20:06:01 +0000

    lesson2

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,14 +1,14 @@
 import os
-from flask  import Flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env
-
-app= Flask(__name__)
-app.config["SECRET_KEY"]= os.environ.get("SECRET_KEY")
-app.config["SQLAlchemy_DATABASE_URI"]=os.environ.get("DB_URL")
+    import env  # noqa
 
 
-db=SQLAlchemy(app)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-from taskmanager import routes
+db = SQLAlchemy(app)
+
+from taskmanager import routes  # noqa
